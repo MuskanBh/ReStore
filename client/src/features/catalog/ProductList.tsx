c38ca852bc2555722b65c4ff23959252cb9df9b6
Muskan_Bharalawala <Muskan_Bharalawala@dell.com>
2023-04-07 17:37:27 +0530

    Adding Routing and Product Details

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 export default function ProductList({products} : Props){
     return(
-        <Grid container spacing={4}>
+        <Grid container spacing={3}>
             {products.map(product=>(
                 <Grid item xs={3} key={product.id}>
                     <ProductCard key={product.id} product={product}/>
