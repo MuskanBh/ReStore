d50a6b1de2471a0072a241f833f6b9548af27c75
Muskan_Bharalawala <Muskan_Bharalawala@dell.com>
2023-04-08 19:27:29 +0530

    Error Handling

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
+import agent from "../../app/api/agent";
+import LoadingIndicator from "../../app/layout/LoadingIndicator";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 
 
 export default function Catalog() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading]= useState(true);
   useEffect(() => {
-    fetch("http://localhost:5056/api/products")
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
+   agent.Catalog.list().then(products =>setProducts(products))
+   .catch(error => console.log(error))
+   .finally(()=> setLoading(false))
   }, []);
+  if(loading) return <LoadingIndicator/>
   return (
     <>
       <ProductList products={products} />
