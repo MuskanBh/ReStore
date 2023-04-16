1df2a97303801d3d026e49b0e7862a5a2bda58fa
Muskan_Bharalawala <Muskan_Bharalawala@dell.com>
2023-04-16 22:24:38 +0530

    Paging, Sorting, Searching

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,8 @@
+using System.Text.Json;
 using API.Data;
 using API.Entities;
+using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,15 +18,32 @@ namespace API.Controllers
 
 
         }
-        [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        [HttpGet] // uses query string by default if string, expects body if it is an object
+        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery]ProductParams productParams)
         {
-            return await _context.Products.ToListAsync();
+            var query=  _context.Products
+                .Sort(productParams.OrderBy)
+                .Search(productParams.SearchTerm)
+                .Filter(productParams.Brands, productParams.Types)
+                .AsQueryable();
+            
+            var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumner, productParams.PageSize);
+            Response.AddPaginationHeader(products.MetaData);
+            return products;
         }
         [HttpGet("{id}")] // api/products/1
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
             return await _context.Products.FindAsync(id);
+        }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters()
+        {
+            var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+            var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
+
+            return Ok(new { brands, types });
         }
     }
 
--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,17 +1,24 @@
 import { Grid } from "@mui/material";
 import { Product } from "../../app/models/product";
 import ProductCard from "./ProductCard";
+import ProductCardSkeleton from "./ProductCardSkeleton";
+import { useAppSelector } from "../../app/store/configureStore";
 
 
 interface Props {
     products:Product[];
 }
 export default function ProductList({products} : Props){
+    const { productsLoaded } = useAppSelector(state => state.catalog);
     return(
         <Grid container spacing={3}>
             {products.map(product=>(
-                <Grid item xs={3} key={product.id}>
-                    <ProductCard key={product.id} product={product}/>
+                <Grid key={product.id} item xs={4}>
+                {!productsLoaded ? (
+                    <ProductCardSkeleton />
+                ) : (
+                    <ProductCard product={product} />
+                )}
                 </Grid>
             ))}
         </Grid>
